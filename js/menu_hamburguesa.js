8d767785c7b0cb4791955bdd78314eec79953e9e
Facu12399 <facundonahuelmaza@gmail.com>
2024-06-06 18:36:14 -0300

    cambios-g

--- js/menu_hamburguesa.js
+++ js/menu_hamburguesa.js
@@ -8,16 +8,6 @@ hamburguesa.addEventListener('click', () => {
         menu_desplegable.style.display = 'none';
     } else{
         menu_desplegable.style.display = 'flex';
-        const catalogo = document.getElementById('catalogo');
-        const catalogo_desplegable = document.getElementById('catalogo-desplegable');
-
-        catalogo.addEventListener('click', ()=>{
-            if(catalogo_desplegable.style.display == 'flex'){
-                catalogo_desplegable.style.display = 'none';
-            } else{
-                catalogo_desplegable.style.display = 'flex';
-            }
-        })
     }
 })
 
