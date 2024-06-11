25de512dcf3d7a77813a8c295216c1fed73ceb49
Facu12399 <facundonahuelmaza@gmail.com>
2024-06-10 23:01:51 -0300

    listo

---
+++ js/slide.js
@@ -0,0 +1,25 @@
+// const flechaIzquierda = document.getElementById('flecha-izquierda');
+// const flechaDerecha = document.getElementById('flecha-derecha');
+// const galeria = document.querySelector('.galeria-grid-mas-vendidos');
+
+// // Manejadores de eventos para las flechas
+// flechaIzquierda.addEventListener('click', () => {
+//     galeria.style.transform = 'translateX(-100%)';
+// });
+
+// flechaDerecha.addEventListener('click', () => {
+//     galeria.style.transform = 'translateX(100%)';
+// });
+
+// const container = document.querySelector('.galeria-grid-mas-vendidos');
+//         const flechaIzquierda = document.getElementById('flecha-izquierda');
+//         const flechaDerecha = document.getElementById('flecha-derecha');
+
+//         // Agrega eventos de clic para las flechas
+//         flechaIzquierda.addEventListener('click', () => {
+//             container.scrollLeft -= 150; // Desplazamiento hacia la izquierda
+//         });
+
+//         flechaDerecha.addEventListener('click', () => {
+//             container.scrollLeft += 150; // Desplazamiento hacia la derecha
+//         });
